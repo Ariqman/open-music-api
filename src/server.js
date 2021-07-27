7a767ea0cff44645f0088e3d920f94d301d99d62
Ariqman <ariqnurmansyah1@gmail.com>
2021-07-27 16:10:12 +0700

    Adding Users Initial Commit

--- src/server.js
+++ src/server.js
@@ -1,11 +1,18 @@
 require('dotenv').config();
 const Hapi = require('@hapi/hapi');
+// Notes Plugin
 const music = require('./api/music');
 const MusicService = require('./services/postgres/musicService');
 const MusicValidator = require('./validator/music');
 
+// Users Plugin
+const users = require('./api/users');
+const UsersService = require('./services/postgres/usersService');
+const UsersValidator = require('./validator/users');
+
 const init = async () => {
   const musicService = new MusicService();
+  const usersService = new UsersService();
   const server = Hapi.server({
     port: process.env.PORT,
     host: process.env.HOST,
@@ -16,14 +23,22 @@ const init = async () => {
     },
   });
 
-  await server.register({
-    plugin: music,
-    options: {
-      service: musicService,
-      validator: MusicValidator,
+  await server.register([
+    {
+      plugin: music,
+      options: {
+        service: musicService,
+        validator: MusicValidator,
+      },
     },
-  });
-
+    {
+      plugin: users,
+      options: {
+        service: usersService,
+        validator: UsersValidator,
+      },
+    },
+  ]);
   await server.start();
   console.log(`Server berjalan pada ${server.info.uri}`);
 };
